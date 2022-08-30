fa894600c367e332979cce6e854517871bfa725c
zama456 <77655925+zama456@users.noreply.github.com>
2022-08-30 11:40:09 +0530

    Google Assistant App

---
+++ app/src/main/java/com/zama/googleassistant/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zama.googleassistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
